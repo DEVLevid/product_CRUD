81e858968389979e89c107b717d8c4ce30c6999f
DEVLevid <levidlima80@gmail.com>
2024-08-11 22:50:16 -0300

    refactor: Refacotring login page for better legibility.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,27 +3,53 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 
+interface LoginState {
+  taxNumber: string;
+  password: string;
+  error: string | null;
+}
+
+const authenticateUser = async (
+  taxNumber: string,
+  password: string
+): Promise<string> => {
+  const response = await axios.post(
+    "https://interview.t-alpha.com.br/api/auth/login",
+    { taxNumber, password }
+  );
+  return response.data.data.token;
+};
+
 export default function Login() {
-  const [taxNumber, setTaxNumber] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [loginState, setLoginState] = useState<LoginState>({
+    taxNumber: "",
+    password: "",
+    error: null,
+  });
   const navigate = useNavigate();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post(
-        "https://interview.t-alpha.com.br/api/auth/login",
-        {
-          taxNumber,
-          password,
-        }
+      const token = await authenticateUser(
+        loginState.taxNumber,
+        loginState.password
       );
-      localStorage.setItem("authToken", response.data.data.token);
+      localStorage.setItem("authToken", token);
       navigate("/products");
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        console.error("Erro ao fazer login:", error.response.data);
-      } else {
-        console.error("Erro ao fazer login:", error);
+        setLoginState((prevState) => ({
+          ...prevState,
+          error: "Não foi possível realizar o login, verifique seus dados",
+        }));
       }
     }
   };
@@ -35,16 +61,20 @@ export default function Login() {
         <input
           className={styles.textInput}
           type="text"
+          name="taxNumber"
           placeholder="Digite seu CPF ou CNPJ"
-          value={taxNumber}
-          onChange={(e) => setTaxNumber(e.target.value)}
+          value={loginState.taxNumber}
+          onChange={handleInputChange}
+          required
         />
         <input
           className={styles.textInput}
           type="password"
+          name="password"
           placeholder="Digite sua Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={loginState.password}
+          onChange={handleInputChange}
+          required
         />
         <button
           className={styles.btnContainer}
@@ -53,6 +83,9 @@ export default function Login() {
         >
           entrar
         </button>
+        {loginState.error && (
+          <p className={styles.error}>{loginState.error}</p>
+        )}
       </div>
       <div className={styles.linkContainer}>
         <a href="/Register">registre-se</a>
